d3275856372dd2c6396a53b4c6eb5ae123c0a4b3
karro <atvars.karro@gmail.com>
2020-12-02 19:04:20 +0200

    add point calc and direction map

--- helpers.py
+++ helpers.py
@@ -1,2 +1,22 @@
 def manhattan(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def add_delta(point, delta):
+    return tuple(sum(x) for x in zip(point, delta))
+
+
+DIRS = {
+    'U': (0, -1),
+    'D': (0, 1),
+    'L': (-1, 0),
+    'R': (1, 0)
+}
+
+
+COMPASS = {
+    'N': (0, -1),
+    'S': (0, 1),
+    'W': (-1, 0),
+    'E': (1, 0)
+}
