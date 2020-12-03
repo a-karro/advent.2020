8294fbc1a13b9627b707a93da502d771eddf337b
karro <atvars.karro@gmail.com>
2020-12-03 11:46:07 +0200

    day 1 on -speeeeeeeed-

--- day01.py
+++ day01.py
@@ -1,9 +1,9 @@
 from itertools import combinations
 
 with open('data/day01.txt', 'r') as f:
-    items = [int(i) for i in f.readlines()]
+    items = set([int(i) for i in f.readlines()])
 
-twos = [t[0] * t[1] for t in combinations(items, 2) if sum(t) == 2020]
-threes = [t[0] * t[1] * t[2] for t in combinations(items, 3) if sum(t) == 2020]
-print("Puzzle 1.1: ", twos[0])
-print("Puzzle 1.2: ", threes[0])
+twos = next(a * b for a, b in combinations(items, 2) if a + b == 2020)
+threes = next(a * b * c for a, b, c in combinations(items, 3) if a + b + c == 2020)
+print("Puzzle 1.1: ", twos)
+print("Puzzle 1.2: ", threes)
