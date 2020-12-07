6b5a5bb43c8b5c9f638ff9c9a86490fb30b1280b
Atvars Karro <atvars.karro@gmail.com>
2020-12-07 11:23:48 +0200

    day 6, minor changes

--- day06.py
+++ day06.py
@@ -1,4 +1,4 @@
 groups = [a.split('\n') for a in open('data/day06.txt', 'r').read().split('\n\n')]
 
-print("Puzzle 6.1: ", sum([len(set(''.join(g))) for g in groups]))
-print("Puzzle 6.2: ", sum([len(set.intersection(*[set(x) for x in g])) for g in groups]))
+print("Puzzle 6.1: ", sum(len(set(''.join(g))) for g in groups))
+print("Puzzle 6.2: ", sum(len(set.intersection(*[set(x) for x in g])) for g in groups))
